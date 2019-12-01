5575acb428e75fd3b0e49c5ee851b91d43280555
Muhamamd Taha Farooq <mtahafrq@gmail.com>
2019-12-01 20:14:19 +0000

    Add programmatic navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import history from '../history';
 
 import Header from './Header'
 
 const App = () => {
     return (
     <div className="ui container">
-        <BrowserRouter>
+        <Router history={history}>
         <Header/>
         <div>
             <Route path="/" exact component={StreamList}></Route>
             <Route path="/streams/new" exact component={StreamCreate}></Route>
-            <Route path="/streams/edit" exact component={StreamEdit}></Route>
+            <Route path="/streams/edit/:id" exact component={StreamEdit}></Route>
             <Route path="/streams/delete" exact component={StreamDelete}></Route>
             <Route path="/streams/show" exact component={StreamShow}></Route>
         </div>
-        </BrowserRouter>
+        </Router>
     </div>);
 }
 
