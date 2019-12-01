08ff71eeb2b5fb0592f3b6a3d44bdf928079c483
Muhamamd Taha Farooq <mtahafrq@gmail.com>
2019-12-01 10:22:19 +0000

    Added streamsReducer and lodash library

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
+
 class StreamCreate extends React.Component {
     renderError ({error, touched}) {
         if (touched && error){
@@ -20,8 +23,8 @@ class StreamCreate extends React.Component {
             </div>
             );
     }
-    onSubmit(formValues){
-        console.log(formValues)
+    onSubmit = (formValues) =>{
+        this.props.createStream(formValues);
     }
     render () {
         return (
@@ -47,7 +50,9 @@ const validate = (formValues) => {
     return errors;
 }
 
-export default reduxForm({
+const formWrapped =  reduxForm({
     form: 'StreamCreate',
     validate
 })(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped);
