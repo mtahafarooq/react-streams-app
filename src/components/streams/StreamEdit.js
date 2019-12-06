31efe24107cc086e48853d40d11df62a05352245
Muhamamd Taha Farooq <mtahafrq@gmail.com>
2019-12-06 18:54:27 +0000

    Added Edit stream functionality

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,6 +1,8 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
-import  { fetchStream } from '../../actions';
+import  { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
 
@@ -8,7 +10,21 @@ class StreamEdit extends React.Component {
         this.props.fetchStream(this.props.match.params.id)
     }
     render () {
-        return <div>StreamEdit</div>
+        if (!this.props.stream){
+            return(<div>Loading...</div>)
+        }
+        return(
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm 
+                    initialValues={_.pick(this.props.stream,'title', 'description')} 
+                    onSubmit={this.onSubmit}
+                />
+            </div>
+        ); 
+    }
+    onSubmit = (formValues) => { 
+        this.props.editStream(this.props.match.params.id, formValues)
     }
 }
 
@@ -16,4 +32,4 @@ class StreamEdit extends React.Component {
 const mapStateToProps = (state, ownProps) => {
     return {stream: state.streams[ownProps.match.params.id]}
 }
-export default connect(mapStateToProps, {fetchStream})(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream})(StreamEdit);
